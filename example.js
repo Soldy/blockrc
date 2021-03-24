eecf74edf80f6d1d8ef374df21f3e41b7ff60b5d
Soldy <4786022+Soldy@users.noreply.github.com>
2021-03-24 06:14:10 +0000

    cleaner code
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example.js
+++ example.js
@@ -3,8 +3,12 @@
 
 const cache = new (require('./cache.js')).Base();
 
-cache.create(1024, 1024);
-cache.setString('cool', 10);
+console.log(
+    cache.create(1024, 1024)
+);
+console.log(
+    cache.setString('cool', 10)
+);
 console.log(
     cache.getString(10)
 );
